e684fd502b76afbb1edac9797dceb468a485167f
d5ng <dong5ffice@gmail.com>
2023-01-31 21:51:38 +0900

    mongoose One to Few

--- models/user.js
+++ models/user.js
@@ -7,5 +7,52 @@ mongoose.connect('mongodb://localhost:27017/relationshipDemo')
 	})
 	.catch((error => console.log(error)));
 
+const userSchema = new mongoose.Schema({
+	first: String,
+	last: String,
+	addresses: [
+		{
+			street: String,
+			city: String,
+			state: String,
+			country: String,
+		}
+	]
+})
 
-	// Hello;
+const User = mongoose.model('User', userSchema);
+
+const makeUser = async () => {
+	const u = new User({
+		first: 'Harry',
+		last: 'Portter'
+	})
+
+	u.addresses.push({
+		street: '123 sesame St.',
+		city: 'New York',
+		state: 'NY',
+		country: 'USA'
+	})
+
+	const res = await u.save();
+	console.log(res);
+}
+
+
+const addAdress = async (id) => {
+	const user = await User.findById(id);
+
+	user.addresses.push({
+		street: '99 3rd St.',
+		city: 'New York',
+		state: 'NY',
+		country: 'USA'
+	})
+
+	const res = await user.save();
+	console.log(res);
+}
+
+// makeUser();
+addAdress('63d90e13e5bc5cc6e5f673d4');
