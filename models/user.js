be5862b354e105310a7f1b6c5840c57617e73c88
d5ng <dong5ffice@gmail.com>
2023-01-31 21:31:51 +0900

    mongoose One to Few

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://localhost:27017/relationshipDemo')
+	.then(() => {
+		console.log('Connection Open');
+	})
+	.catch((error => console.log(error)));
+
+
+	// Hello;
